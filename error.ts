a09049f1e899828c058121a3fa978876810faf89
atk81 <ashutoshkushwaha81@gmail.com>
2022-10-11 19:36:48 +0530

    Error handling in event listner.

---
+++ error.ts
@@ -0,0 +1,62 @@
+/*
+Error in event emitter is handle by special method.
+
+Problem: You’re dealing with multiple non-blocking APIs, but are struggling to effectively handle errors.
+Solution: Node’s domain module can be used to centralize error handling for a set of asynchronous operations,
+and this includes EventEmitter instances that emit unhandled error events.
+ */
+
+/*
+ * Event Emitter can be created using two method
+ * 1. By inheriting from base class
+ * 2. Mixing: Copy it's method into other classes.
+ * In this example we are going to explore the 1st way of doing things.
+ * Next example: createEventEmitter1.ts.
+ */
+
+const utils = require("util");
+const EventEmitter = require("events").EventEmitter;
+const domain = require('domain');
+const audioDomain = domain.create();
+
+
+class AudioDevice extends EventEmitter {
+    constructor() {
+        super();
+        this.on('play', this.play.bind(this));
+    }
+
+    play(){
+        this.emit('error', 'Play is not implemented yet');
+    }
+}
+
+
+
+class MusicPlayer extends EventEmitter {
+    constructor() {
+        super();
+        this.playing = false;
+        this.audioDevice = new AudioDevice();
+        // Just for testing, it should also be handled properly.
+        this.audioDevice.emit('error', 'No audio track found!');
+    };
+
+    play(){
+        this.playing = true;
+        this.audioDevice = new AudioDevice();
+        console.log(this.audioDevice);
+        this.audioDevice.emit('play');
+        console.log('Now playing');
+    }
+}
+
+
+audioDomain.on('error', (err)=>{
+    console.log(err);
+})
+
+audioDomain.run(function(){
+    const musicPlayer = new MusicPlayer();
+    musicPlayer.play();
+});
